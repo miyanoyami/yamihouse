ccd59bc571a04d7c1ee7922c9acede956975aaae
miyanoyami <86955059+miyanoyami@users.noreply.github.com>
2021-08-11 19:12:21 +0900

    chore: gallery

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -3,9 +3,38 @@
     <v-col class="text-center">
       <blockquote class="blockquote">
         絵とか貼っていきたいね
-        <footer>
-        </footer>
+
+        <!-- component -->
+        <viewer :images="images">
+          <img v-for="src in images" :key="src" :src="src" width="180px">
+        </viewer>
+
+        <footer></footer>
       </blockquote>
     </v-col>
   </v-row>
 </template>
+
+<script>
+import 'viewerjs/dist/viewer.css'
+import VueViewer from 'v-viewer'
+import Vue from 'vue'
+Vue.use(VueViewer)
+export default {
+  data () {
+    return {
+      images: [
+        '/yami.jpeg',
+        '/yami_full.png'
+      ]
+    }
+  },
+  methods: {
+    show () {
+      this.$viewerApi({
+        images: this.images
+      })
+    }
+  }
+}
+</script>
--- pages/game.vue
+++ pages/game.vue
@@ -2,15 +2,38 @@
   <v-container>
     <v-row>
       <v-col class="text-center">
-        <p class="h2">ゲームについて</p>
+        <p class="h2">ゲームについて </p>
       </v-col>
     </v-row>
     <v-row>
       <v-col class="text-center">
         <p>コンシューマーゲームを中心に幅広くカジュアルに遊んでます。</p>
-        <p>PCゲーはあんまりやってないけど興味あり、スマホゲームはあまりやりません。</p>
+        <p>
+          PCゲーはあんまりやってないけど興味あり、スマホゲームはあまりやりません。
+        </p>
         <p>あと実はボードゲームが好きです。</p>
-        <p>楽しそうなら迷わず買うのがポリシーです。</p>
+
+        <p class="h3">よくやってるゲーム</p>
+        <v-card class="mx-auto" max-width="400" tile>
+          <v-list-item two-line>
+            <v-list-item-content>
+              <v-list-item-title>スプラトゥーン2</v-list-item-title>
+              <v-list-item-subtitle>全ルールX22</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item two-line>
+            <v-list-item-content>
+              <v-list-item-title>EXVSマキシブーストオン</v-list-item-title>
+              <v-list-item-subtitle>シャフ少将☆,固定中尉</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item two-line>
+            <v-list-item-content>
+              <v-list-item-title>モンスターハンターライズ</v-list-item-title>
+              <v-list-item-subtitle>HR230~</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-card>
 
         <footer></footer>
       </v-col>
@@ -18,5 +41,3 @@
   </v-container>
 </template>
 
-<script>
-</script>
