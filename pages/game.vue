faf1295327fe1220ec4891b8929a1b4d0f48454b
miyanoyami <86955059+miyanoyami@users.noreply.github.com>
2021-08-18 19:48:40 +0900

    chore: game list

--- pages/game.vue
+++ pages/game.vue
@@ -2,45 +2,85 @@
   <v-container>
     <v-row>
       <v-col class="text-center">
-        <p class="text-h5">
-          ゲームについて
-        </p>
+        <p class="text-h5">ゲームについて</p>
       </v-col>
     </v-row>
     <v-row>
       <v-col class="text-center">
         <p>コンシューマーゲームを中心に幅広くカジュアルに遊んでます。</p>
-        <p>
-          PCゲームはあまりやってないけど興味あります。
-        </p>
-        <p>
-          スマホゲームはあまりやりません。
-        </p>
+        <p>PCゲームはあんまりやってないけど興味あります。</p>
+        <p>スマホゲームはあまりやりません。</p>
         <p>あと実はボードゲームが好きです。</p>
 
-        <p class="text-h6">
-          よくやってるゲーム
-        </p>
-        <v-card class="mx-auto" max-width="400" tile>
-          <v-list-item two-line>
-            <v-list-item-content>
-              <v-list-item-title>スプラトゥーン2</v-list-item-title>
-              <v-list-item-subtitle>全ルールX22</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item two-line>
-            <v-list-item-content>
-              <v-list-item-title>EXVSマキシブーストオン</v-list-item-title>
-              <v-list-item-subtitle>シャフ少将☆,固定中尉</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item two-line>
-            <v-list-item-content>
-              <v-list-item-title>モンスターハンターライズ</v-list-item-title>
-              <v-list-item-subtitle>HR230~</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-        </v-card>
+        <v-list dense>
+          <v-subheader>よくやってるゲーム</v-subheader>
+          <v-list-item-group>
+            <v-list-item two-line>
+              <v-list-item-content>
+                <v-list-item-title>スプラトゥーン2</v-list-item-title>
+                <v-list-item-subtitle>全ルールX22</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item two-line>
+              <v-list-item-content>
+                <v-list-item-title>EXVSマキシブーストオン</v-list-item-title>
+                <v-list-item-subtitle>シャフ少将☆,固定中尉</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item two-line>
+              <v-list-item-content>
+                <v-list-item-title>モンスターハンターライズ</v-list-item-title>
+                <v-list-item-subtitle>HR230~</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item two-line>
+              <v-list-item-content>
+                <v-list-item-title>アークザラッド</v-list-item-title>
+                <v-list-item-subtitle>1クリア、2進行中</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item two-line>
+              <v-list-item-content>
+                <v-list-item-title>Minecraft</v-list-item-title>
+                <v-list-item-subtitle>初心者</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item two-line>
+              <v-list-item-content>
+                <v-list-item-title>among us</v-list-item-title>
+                <v-list-item-subtitle></v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+
+        <v-list dense>
+          <v-subheader class="text-center">持ってる・たまにやるゲーム</v-subheader>
+          <v-list-item-group>
+            <v-list-item two-line>
+              <v-list-item-content>
+                <v-list-item-title>ポケモンユナイト</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item two-line>
+              <v-list-item-content>
+                <v-list-item-title>GodField</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item two-line>
+              <v-list-item-content>
+                <v-list-item-title>桃太郎電鉄 switch</v-list-item-title>
+                <v-list-item-subtitle></v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item two-line>
+              <v-list-item-content>
+                <v-list-item-title>ボンバーマンRオンライン</v-list-item-title>
+                <v-list-item-subtitle></v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
 
         <footer></footer>
       </v-col>
